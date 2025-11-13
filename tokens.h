bca9431fe6e64d316a97e25cdc7a64aa2202f290
tylerhuynh4 <tylerhuynh92@csu.fullerton.edu>
2025-11-12 23:14:31 -0800

    comments for clarity and restructure

--- tokens.h
+++ tokens.h
@@ -3,7 +3,8 @@
 #include <unordered_set>
 #include <regex>
 
-inline std::string classifyToken(const std::string &token) {
+inline std::string classifyToken(const std::string &token) {        // inline allows compile in header file w/o multiple definition errors
+
     static const std::unordered_set<std::string> keywords = {
         "let", "const", "function", "return", "if", "else", "for", "while", "var", "true", "false"
     };
@@ -15,8 +16,9 @@ inline std::string classifyToken(const std::string &token) {
     static const std::unordered_set<std::string> separators = {
         "(", ")", "{", "}", ";", ",", "[", "]"
     };
-
-    if (keywords.count(token)) {
+    
+    // checks if token is known or matches from set
+    if (keywords.count(token)) {    
         return "keyword";
     }
     if (operators.count(token)) {
@@ -25,7 +27,7 @@ inline std::string classifyToken(const std::string &token) {
     if (separators.count(token)) {
         return "separator";
     }
-    if (std::regex_match(token, std::regex(R"(\d+)"))) {
+    if (std::regex_match(token, std::regex(R"(\d+)"))) {    // purely digits
         return "literal";
     }
     if (std::regex_match(token, std::regex(R"(["'][^"']*["'])"))) {
